002707f999b98e7c3662a4c1c78742cc7e739175
Dillon Lareau <flybye22@gmail.com>
2017-09-25 18:53:32 -0400

    Reversed puzzle unlocking in admin, Fixes #22

--- huntserver/admin.py
+++ huntserver/admin.py
@@ -10,13 +10,37 @@ class UnlockableInline(admin.TabularInline):
     model = models.Unlockable
     extra = 1
 
+class UnlockInline(admin.TabularInline):
+    model = models.Puzzle.unlocks.through
+    extra = 2
+    fk_name = 'to_puzzle'
+    verbose_name = "Puzzle that counts towards unlocking this puzzle"
+    verbose_name_plural = "Puzzles that count towards this puzzle"
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "from_puzzle":
+            try:
+                parent_obj_id = request.resolver_match.args[0]
+                puzzle = models.Puzzle.objects.get(id=parent_obj_id)
+                kwargs["queryset"] = models.Puzzle.objects.filter(hunt=puzzle.hunt).order_by('puzzle_id')
+            except IndexError:
+                pass
+        return super(UnlockInline, self).formfield_for_foreignkey(db_field, request, **kwargs)
+
 class PuzzleAdmin(admin.ModelAdmin):
+    def get_object(self, request, object_id, to_field):
+        # Hook obj for use in formfield_for_manytomany
+        self.obj = super(PuzzleAdmin, self).get_object(request, object_id, to_field)
+        return self.obj
+
     def formfield_for_manytomany(self, db_field, request, **kwargs):
-        if db_field.name == "unlocks":
-            kwargs["queryset"] = models.Puzzle.objects.filter(hunt=models.Hunt.objects.get(is_current_hunt=True)).order_by('puzzle_id')
+        if db_field.name == "unlocks" and getattr(self, 'obj', None):
+            kwargs["queryset"] = models.Puzzle.objects.filter(hunt=self.obj.hunt).order_by('puzzle_id')
         return super(PuzzleAdmin, self).formfield_for_manytomany(db_field, request, **kwargs)
     list_filter = ('hunt',)
-    filter_horizontal = ('unlocks',)
+    fields = ('hunt', 'puzzle_name', 'puzzle_number', 'puzzle_id', 'answer',
+              'link', 'num_pages', 'num_required_to_unlock')
+    inlines = (UnlockInline,)
 
 class TeamAdminForm(forms.ModelForm):
     persons = forms.ModelMultipleChoiceField(
