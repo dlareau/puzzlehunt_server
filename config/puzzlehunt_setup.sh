ef1c8311b19d5376bca107d96291201d949cf270
Dillon Lareau <flybye22@gmail.com>
2018-06-19 18:20:26 -0400

    updated the vagrant related setup script for dev

--- config/puzzlehunt_setup.sh
+++ config/puzzlehunt_setup.sh
@@ -8,44 +8,41 @@
 # Variables
 MYSQL_ROOT_PASSWORD=wrongbaa
 MYSQL_NORMAL_USER=hunt
-MYSQL_NORMAL_PASSWORD=puzzlehunt
+MYSQL_NORMAL_PASSWORD=$(head /dev/urandom | LC_ALL=C tr -dc 'A-Za-z0-9!@#$%^&*(\-_=+)' | head -c 16)
 MYSQL_PUZZLEHUNT_DB=puzzlehunt_db
 
-# Make sure we don't get prompted for anything
-export DEBIAN_FRONTEND="noninteractive"
-debconf-set-selections <<< "mysql-server mysql-server/root_password password $MYSQL_ROOT_PASSWORD"
-debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $MYSQL_ROOT_PASSWORD"
+# Helper functions
+yell() { echo "$0: $*" >&2; }
+die() { yell "$*"; exit 111; }
+try() { "$@" || die "cannot $*"; }
 
-# Get all basic system packages
+# Need git to kick off the process
 apt-get update
 apt-get install -y git
-apt-get install -y mysql-client
-apt-get install -y mysql-server
-apt-get install -y libmysqlclient-dev
-apt-get install -y python-dev
-apt-get install -y python-mysqldb
-apt-get install -y python-pip
 
 # Get the git repository and link it for external access
-cd /vagrant
-rm -rf puzzlehunt_server
-git clone https://github.com/dlareau/puzzlehunt_server.git
-ln -s /vagrant/puzzlehunt_server /home/vagrant/puzzlehunt_server
-cd /home/vagrant/puzzlehunt_server
+try cd /vagrant
+try ln -s /vagrant/puzzlehunt_server /home/vagrant/puzzlehunt_server
+try cd /home/vagrant/puzzlehunt_server
+try git checkout development # Only needed until test branch is merged
 
-# Get all python dependencies and setup virtual environment
-pip install virtualenv
-virtualenv venv
-source venv/bin/activate
-pip install -r requirements.txt
+# Make sure we don't get prompted for anything
+try export DEBIAN_FRONTEND="noninteractive"
+try debconf-set-selections <<< "mysql-server mysql-server/root_password password $MYSQL_ROOT_PASSWORD"
+try debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $MYSQL_ROOT_PASSWORD"
+
+# Get all basic system packages
+try apt-get install -y mysql-client mysql-server libmysqlclient-dev python-dev python-mysqldb python-pip apache2 libapache2-mod-xsendfile libapache2-mod-wsgi imagemagick
+
+apt-get install -y libapache2-mod-proxy-html || true
 
 # Set up MYSQL user and database
-mysql -uroot -p$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE $MYSQL_PUZZLEHUNT_DB"
-mysql -uroot -p$MYSQL_ROOT_PASSWORD -e "grant all privileges on $MYSQL_PUZZLEHUNT_DB.* to '$MYSQL_NORMAL_USER'@'localhost' identified by '$MYSQL_NORMAL_PASSWORD'"
+try mysql -uroot -p$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE IF NOT EXISTS $MYSQL_PUZZLEHUNT_DB"
+try mysql -uroot -p$MYSQL_ROOT_PASSWORD -e "grant all privileges on $MYSQL_PUZZLEHUNT_DB.* to '$MYSQL_NORMAL_USER'@'localhost' identified by '$MYSQL_NORMAL_PASSWORD'"
 
 # Configure application (Consider this the same as modifying secret_settings.py.template)
-cat > puzzlehunt_server/secret_settings.py <<EOF
-SECRET_KEY = 'this is not the secret key, use your own'
+try cat > puzzlehunt_server/secret_settings.py <<EOF
+SECRET_KEY = '$(head /dev/urandom | LC_ALL=C tr -dc 'A-Za-z0-9!@#$%^&*(\-_=+)' | head -c 50)'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -62,25 +59,31 @@ EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = ''
 EOF
 
+
+# Get all python dependencies and setup virtual environment
+try pip install virtualenv
+try virtualenv venv
+try source venv/bin/activate
+try pip install -r requirements.txt
+
 # Run application setup commands
-python manage.py migrate
-python manage.py collectstatic --noinput
-git checkout development # Only needed until test branch is merged
-python manage.py loaddata config/initial_hunt.json
+try mkdir -p ./media/puzzles
+try python manage.py migrate
+try python manage.py collectstatic --noinput
+try python manage.py loaddata config/initial_hunt.json
+try deactivate
 
 # We are root until this point, pass off ownership of all we have created
-chown -R vagrant .
+try chown -R vagrant .
+try chmod -R go+r .
+try chmod -R og+rw ./media
 
 # Apache hosting setup
-apt-get install -y apache2
-apt-get install -y libapache2-mod-xsendfile
-apt-get install -y libapache2-mod-proxy-html
-apt-get install -y libapache2-mod-wsgi
-a2enmod proxy
-a2enmod proxy_http
-a2enmod proxy_html
-a2enmod xsendfile
-a2enmod wsgi
+try a2enmod proxy
+try a2enmod proxy_http
+try a2enmod proxy_html
+try a2enmod xsendfile
+try a2enmod wsgi
 rm /etc/apache2/sites-enabled/*
-cp config/puzzlehunt_vagrant.conf /etc/apache2/sites-enabled/
-service apache2 restart
+try cp config/puzzlehunt_vagrant.conf /etc/apache2/sites-enabled/
+try service apache2 restart
