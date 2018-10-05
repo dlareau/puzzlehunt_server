2da8fcfb333c818d128ae7f448d66e0e66d124e6
Dillon Lareau <flybye22@gmail.com>
2018-10-05 14:36:06 -0400

    Info page also tells how many users are new

--- huntserver/staff_views.py
+++ huntserver/staff_views.py
@@ -326,17 +326,26 @@ def hunt_info(request):
     curr_hunt = Hunt.objects.get(is_current_hunt=True)
     teams = curr_hunt.real_teams
     people = []
+    new_people = []
     for team in teams:
         people = people + list(team.person_set.all())
+    try:
+        old_hunt = Hunt.objects.get(hunt_number=curr_hunt.hunt_number-1)
+        new_people = [p for p in people if p.user.date_joined > old_hunt.start_date]
+    except:
+        new_people = people
+
     need_teams = teams.filter(location="need_a_room") | teams.filter(location="needs_a_room")
     have_teams = teams.exclude(location="need_a_room").exclude(location="needs_a_room").exclude(location="off_campus")
     offsite_teams = teams.filter(location="off_campus")
 
     context = {'curr_hunt': curr_hunt, 
                'people': people,
+               'new_people': new_people,
                'need_teams': need_teams.all(),
                'have_teams': have_teams.all(),
-               'offsite_teams': offsite_teams.all()}
+               'offsite_teams': offsite_teams.all(),
+            }
     return render(request, 'staff_hunt_info.html', context)
 
 
