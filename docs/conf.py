4e742e4ad23ed7d254b74bc0e7a47826c4560908
Dillon Lareau <flybye22@gmail.com>
2019-01-11 21:49:53 -0500

    Fix doc building

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ from django.conf import settings
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 # sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('..'))
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'puzzlehunt_server.settings.local_settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'puzzlehunt_server.settings.travis_settings')
 django.setup()
 
 # -- General configuration ------------------------------------------------
