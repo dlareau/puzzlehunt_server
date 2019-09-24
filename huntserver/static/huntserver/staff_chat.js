ceee938bbb5df074d139e511d9488f93acb06d25
Dillon Lareau <flybye22@gmail.com>
2019-09-24 16:38:09 -0400

    Updated look of chat

--- huntserver/static/huntserver/staff_chat.js
+++ huntserver/static/huntserver/staff_chat.js
@@ -6,8 +6,10 @@
       });
       $("#chat_" + $(this).data('id')).show();
       curr_team = $(this).data('id');
+      full_name = $(this).data('full_name');
       $("button[data-id=" + curr_team + "]").addClass('active')
       $("button[data-id=" + curr_team + "]").css("background-color", "");
+      $("#team_label").html("Chatting with: " + full_name);
       $("#chatcontainer").scrollTop($("#chatcontainer")[0].scrollHeight);
     });
   }
