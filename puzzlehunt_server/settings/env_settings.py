5be8c4fd09f7b242b6accd0a2ecfeae0ef9d8ef1
Dillon Lareau <flybye22@gmail.com>
2020-01-26 23:31:41 -0500

    Moved settings to environment variables

--- puzzlehunt_server/settings/env_settings.py
+++ puzzlehunt_server/settings/env_settings.py
@@ -1,19 +1,14 @@
 from .base_settings import *
+import dj_database_url
 import os
 
-DEBUG = os.environ.get("DJANGO_ENABLE_DEBUG") == "True"
+DEBUG = os.environ.get("DJANGO_ENABLE_DEBUG").lower == "true"
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get("DJANGO_DB_NAME"),
-        'HOST': 'db',
-        'PORT': '',
-        'USER': os.environ.get("DJANGO_DB_USER"),
-        'PASSWORD': os.environ.get("DJANGO_DB_PASSWORD"),
-        #'OPTIONS': {'charset': 'utf8mb4'},
-    }
-}
+DATABASES = {'default': dj_database_url.config(conn_max_age=600)}
+
+if(DATABASES['default']['ENGINE'] == 'django.db.backends.mysql'):
+	DATABASES['default']['OPTIONS'] = {'charset': 'utf8mb4'}
+
 INTERNAL_IPS = ['127.0.0.1', 'localhost',]
 EMAIL_HOST_USER = os.environ.get("DJANGO_EMAIL_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("DJANGO_EMAIL_PASSWORD")
