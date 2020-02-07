dfef7764694329e0edb2c7abd104995bab2dd974
Dillon Lareau <jdlareau@cert.org>
2020-02-07 14:31:43 -0500

    PEP8 for models.py

--- huntserver/models.py
+++ huntserver/models.py
@@ -17,9 +17,11 @@ time_zone = tz.gettz(settings.TIME_ZONE)
 class Hunt(models.Model):
     """ Base class for a hunt. Contains basic details about a puzzlehunt. """
 
-    hunt_name = models.CharField(max_length=200,
+    hunt_name = models.CharField(
+        max_length=200,
         help_text="The name of the hunt as the public will see it")
-    hunt_number = models.IntegerField(unique=True,
+    hunt_number = models.IntegerField(
+        unique=True,
         help_text="A number used internally for hunt sorting, must be unique")
     team_size = models.IntegerField()
     start_date = models.DateTimeField(
@@ -30,16 +32,24 @@ class Hunt(models.Model):
         help_text="The start date/time displayed to users")
     display_end_date = models.DateTimeField(
         help_text="The end date/time displayed to users")
-    location = models.CharField(max_length=100,
+    location = models.CharField(
+        max_length=100,
         help_text="Starting location of the puzzlehunt")
-    resource_link = models.URLField(max_length=200, blank=True,
+    resource_link = models.URLField(
+        max_length=200,
+        blank=True,
         help_text="The full link (needs http://) to a folder of additional resources.")
-    is_current_hunt = models.BooleanField(default=False)
-    extra_data = models.CharField(max_length=200, blank=True,
+    is_current_hunt = models.BooleanField(
+        default=False)
+    extra_data = models.CharField(
+        max_length=200,
+        blank=True,
         help_text="A misc. field for any extra data to be stored with the hunt.")
-    template = models.TextField(default="",
+    template = models.TextField(
+        default="",
         help_text="The template string to be rendered to HTML on the hunt page")
-    hint_lockout = models.IntegerField(default=settings.DEFAULT_HINT_LOCKOUT)
+    hint_lockout = models.IntegerField(
+        default=settings.DEFAULT_HINT_LOCKOUT)
 
     # A bit of custom logic in clean and save to ensure exactly one hunt's
     # is_current_hunt is true at any time. It makes sure you can never un-set the
@@ -114,33 +124,54 @@ class Puzzle(models.Model):
 
     puzzle_number = models.IntegerField(
         help_text="The number of the puzzle within the hunt, for sorting purposes")
-    puzzle_name = models.CharField(max_length=200,
+    puzzle_name = models.CharField(
+        max_length=200,
         help_text="The name of the puzzle as it will be seen by hunt participants")
-    puzzle_id = models.CharField(max_length=8, unique=True,  # hex only please
+    puzzle_id = models.CharField(
+        max_length=8,
+        unique=True,  # hex only please
         help_text="A 3-5 character hex string that uniquely identifies the puzzle")
-    answer = models.CharField(max_length=100,
+    answer = models.CharField(
+        max_length=100,
         help_text="The answer to the puzzle, not case sensitive")
-    link = models.URLField(max_length=200, blank=True,
+    link = models.URLField(
+        max_length=200,
+        blank=True,
         help_text="The full link (needs http://) to a publicly accessible PDF of the puzzle")
-    resource_link = models.URLField(max_length=200, blank=True,
+    resource_link = models.URLField(
+        max_length=200,
+        blank=True,
         help_text="The full link (needs http://) to a folder of additional resources.")
-    solution_link = models.URLField(max_length=200, blank=True,
+    solution_link = models.URLField(
+        max_length=200,
+        blank=True,
         help_text="The full link (needs http://) to a publicly accessible PDF of the solution")
-    num_required_to_unlock = models.IntegerField(default=1,
+    num_required_to_unlock = models.IntegerField(
+        default=1,
         help_text="Number of prerequisite puzzles that need to be solved to unlock this puzzle")
-    unlocks = models.ManyToManyField("self", blank=True, symmetrical=False,
+    unlocks = models.ManyToManyField(
+        "self",
+        blank=True,
+        symmetrical=False,
         help_text="Puzzles that this puzzle is a possible prerequisite for")
-    hunt = models.ForeignKey(Hunt, on_delete=models.CASCADE,
+    hunt = models.ForeignKey(
+        Hunt,
+        on_delete=models.CASCADE,
         help_text="The hunt that this puzzle is a part of")
-    extra_data = models.CharField(max_length=200, blank=True,
+    extra_data = models.CharField(
+        max_length=200,
+        blank=True,
         help_text="A misc. field for any extra data to be stored with the puzzle.")
     num_pages = models.IntegerField(
         help_text="Number of pages in the PDF for this puzzle. Set automatically upon download")
-    is_meta = models.BooleanField(default=False,
+    is_meta = models.BooleanField(
+        default=False,
         help_text="Is this puzzle a meta-puzzle?")
-    is_html_puzzle = models.BooleanField(default=False,
+    is_html_puzzle = models.BooleanField(
+        default=False,
         help_text="Does this puzzle use an HTML folder as it's source?")
-    doesnt_count = models.BooleanField(default=False,
+    doesnt_count = models.BooleanField(
+        default=False,
         help_text="Should this puzzle not count towards scoring?")
 
     def serialize_for_ajax(self):
@@ -159,19 +190,30 @@ class Puzzle(models.Model):
 class Prepuzzle(models.Model):
     """ A class representing a pre-puzzle within a hunt """
 
-    puzzle_name = models.CharField(max_length=200,
+    puzzle_name = models.CharField(
+        max_length=200,
         help_text="The name of the puzzle as it will be seen by hunt participants")
-    released = models.BooleanField(default=False)
-    hunt = models.OneToOneField(Hunt, on_delete=models.CASCADE, blank=True, null=True,
+    released = models.BooleanField(
+        default=False)
+    hunt = models.OneToOneField(
+        Hunt,
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
         help_text="The hunt that this puzzle is a part of, leave blank for no associated hunt.")
-    answer = models.CharField(max_length=100,
+    answer = models.CharField(
+        max_length=100,
         help_text="The answer to the puzzle, not case sensitive")
-    template = models.TextField(default=u'{% extends "prepuzzle.html" %}\r\n' +
-        u'{% load prepuzzle_tags %}\r\n\r\n{% block content %}\r\n{% endblock content %}',
+    template = models.TextField(
+        default='{% extends "prepuzzle.html" %}\r\n{% load prepuzzle_tags %}\r\n' +
+                '\r\n{% block content %}\r\n{% endblock content %}',
         help_text="The template string to be rendered to HTML on the hunt page")
-    resource_link = models.URLField(max_length=200, blank=True,
+    resource_link = models.URLField(
+        max_length=200,
+        blank=True,
         help_text="The full link (needs http://) to a folder of additional resources.")
-    response_string = models.TextField(default="",
+    response_string = models.TextField(
+        default="",
         help_text="Data returned to the webpage for use upon solving.")
 
     def __str__(self):
@@ -185,25 +227,45 @@ class Prepuzzle(models.Model):
 class Team(models.Model):
     """ A class representing a team within a hunt """
 
-    team_name = models.CharField(max_length=200,
+    team_name = models.CharField(
+        max_length=200,
         help_text="The team name as it will be shown to hunt participants")
-    solved = models.ManyToManyField(Puzzle, blank=True, related_name='solved_for', through="Solve",
+    solved = models.ManyToManyField(
+        Puzzle,
+        blank=True,
+        related_name='solved_for',
+        through="Solve",
         help_text="The puzzles the team has solved")
-    unlocked = models.ManyToManyField(Puzzle, blank=True, related_name='unlocked_for',
-        through="Unlock", help_text="The puzzles the team has unlocked")
-    unlockables = models.ManyToManyField("Unlockable", blank=True,
+    unlocked = models.ManyToManyField(
+        Puzzle,
+        blank=True,
+        related_name='unlocked_for',
+        through="Unlock",
+        help_text="The puzzles the team has unlocked")
+    unlockables = models.ManyToManyField(
+        "Unlockable",
+        blank=True,
         help_text="The unlockables the team has earned")
-    hunt = models.ForeignKey(Hunt, on_delete=models.CASCADE,
+    hunt = models.ForeignKey(
+        Hunt,
+        on_delete=models.CASCADE,
         help_text="The hunt that the team is a part of")
-    location = models.CharField(max_length=80, blank=True,
+    location = models.CharField(
+        max_length=80,
+        blank=True,
         help_text="The physical location that the team is solving at")
-    join_code = models.CharField(max_length=5,
+    join_code = models.CharField(
+        max_length=5,
         help_text="The 5 character random alphanumeric password needed for a user to join a team")
-    playtester = models.BooleanField(default=False,
+    playtester = models.BooleanField(
+        default=False,
         help_text="A boolean to indicate if the team is a playtest team and will get early access")
-    last_seen_message = models.IntegerField(default=0)
-    last_received_message = models.IntegerField(default=0)
-    num_available_hints = models.IntegerField(default=0)
+    last_seen_message = models.IntegerField(
+        default=0)
+    last_received_message = models.IntegerField(
+        default=0)
+    num_available_hints = models.IntegerField(
+        default=0)
 
     @property
     def is_playtester_team(self):
@@ -243,15 +305,25 @@ class Team(models.Model):
 class Person(models.Model):
     """ A class to associate more personal information with the default django auth user class """
 
-    user = models.OneToOneField(User, on_delete=models.CASCADE,
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
         help_text="The corresponding user to this person")
-    phone = models.CharField(max_length=20, blank=True,
+    phone = models.CharField(
+        max_length=20,
+        blank=True,
         help_text="Person's phone number, no particular formatting")
-    allergies = models.CharField(max_length=400, blank=True,
+    allergies = models.CharField(
+        max_length=400,
+        blank=True,
         help_text="Allergy information for the person")
-    comments = models.CharField(max_length=400, blank=True,
+    comments = models.CharField(
+        max_length=400,
+        blank=True,
         help_text="Comments or other notes about the person")
-    teams = models.ManyToManyField(Team, blank=True,
+    teams = models.ManyToManyField(
+        Team,
+        blank=True,
         help_text="Teams that the person is on")
     is_shib_acct = models.BooleanField(
         help_text="A boolean to indicate if the person uses shibboleth authentication for login")
@@ -268,13 +340,20 @@ class Person(models.Model):
 class Submission(models.Model):
     """ A class representing a submission to a given puzzle from a given team """
 
-    team = models.ForeignKey(Team, on_delete=models.CASCADE,
+    team = models.ForeignKey(
+        Team,
+        on_delete=models.CASCADE,
         help_text="The team that made the submission")
     submission_time = models.DateTimeField()
-    submission_text = models.CharField(max_length=100)
-    response_text = models.CharField(blank=True, max_length=400,
+    submission_text = models.CharField(
+        max_length=100)
+    response_text = models.CharField(
+        blank=True,
+        max_length=400,
         help_text="Response to the given answer. Empty string indicates human response needed")
-    puzzle = models.ForeignKey(Puzzle, on_delete=models.CASCADE,
+    puzzle = models.ForeignKey(
+        Puzzle,
+        on_delete=models.CASCADE,
         help_text="The puzzle that this submission is in response to")
     modified_date = models.DateTimeField(
         help_text="Last date/time of response modification")
@@ -310,11 +389,18 @@ class Submission(models.Model):
 class Solve(models.Model):
     """ A class that links a team and a puzzle to indicate that the team has solved the puzzle """
 
-    puzzle = models.ForeignKey(Puzzle, on_delete=models.CASCADE,
+    puzzle = models.ForeignKey(
+        Puzzle,
+        on_delete=models.CASCADE,
         help_text="The puzzle that this is a solve for")
-    team = models.ForeignKey(Team, on_delete=models.CASCADE,
+    team = models.ForeignKey(
+        Team,
+        on_delete=models.CASCADE,
         help_text="The team that this solve is from")
-    submission = models.ForeignKey(Submission, blank=True, on_delete=models.CASCADE,
+    submission = models.ForeignKey(
+        Submission,
+        blank=True,
+        on_delete=models.CASCADE,
         help_text="The submission object that the team submitted to solve the puzzle")
 
     class Meta:
@@ -344,9 +430,13 @@ class Solve(models.Model):
 class Unlock(models.Model):
     """ A class that links a team and a puzzle to indicate that the team has unlocked the puzzle """
 
-    puzzle = models.ForeignKey(Puzzle, on_delete=models.CASCADE,
+    puzzle = models.ForeignKey(
+        Puzzle,
+        on_delete=models.CASCADE,
         help_text="The puzzle that this is an unlock for")
-    team = models.ForeignKey(Team, on_delete=models.CASCADE,
+    team = models.ForeignKey(
+        Team,
+        on_delete=models.CASCADE,
         help_text="The team that this unlocked puzzle is for")
     time = models.DateTimeField(
         help_text="The time this puzzle was unlocked for this team")
@@ -369,11 +459,14 @@ class Unlock(models.Model):
 class Message(models.Model):
     """ A class that represents a message sent using the chat functionality """
 
-    team = models.ForeignKey(Team, on_delete=models.CASCADE,
+    team = models.ForeignKey(
+        Team,
+        on_delete=models.CASCADE,
         help_text="The team that this message is being sent to/from")
     is_response = models.BooleanField(
         help_text="A boolean representing whether or not the message is from the staff")
-    text = models.CharField(max_length=400,
+    text = models.CharField(
+        max_length=400,
         help_text="Message text")
     time = models.DateTimeField(
         help_text="Message send time")
@@ -392,11 +485,17 @@ class Unlockable(models.Model):
         ('TXT', 'Text'),
         ('WEB', 'Link'),
     )
-    puzzle = models.ForeignKey(Puzzle, on_delete=models.CASCADE,
+    puzzle = models.ForeignKey(
+        Puzzle,
+        on_delete=models.CASCADE,
         help_text="The puzzle that needs to be solved to unlock this object")
-    content_type = models.CharField(max_length=3, choices=TYPE_CHOICES, default='TXT',
+    content_type = models.CharField(
+        max_length=3,
+        choices=TYPE_CHOICES,
+        default='TXT',
         help_text="The type of object that is to be unlocked, can be 'IMG', 'PDF', 'TXT', or 'WEB'")
-    content = models.CharField(max_length=500,
+    content = models.CharField(
+        max_length=500,
         help_text="The link to the content, files must be externally hosted.")
 
     def __str__(self):
@@ -407,11 +506,15 @@ class Unlockable(models.Model):
 class Response(models.Model):
     """ A class to represent an automated response regex """
 
-    puzzle = models.ForeignKey(Puzzle, on_delete=models.CASCADE,
+    puzzle = models.ForeignKey(
+        Puzzle,
+        on_delete=models.CASCADE,
         help_text="The puzzle that this automated response is related to")
-    regex = models.CharField(max_length=400,
+    regex = models.CharField(
+        max_length=400,
         help_text="The python-style regex that will be checked against the user's response")
-    text = models.CharField(max_length=400,
+    text = models.CharField(
+        max_length=400,
         help_text="The text to use in the submission response if the regex matched")
 
     def __str__(self):
@@ -422,17 +525,26 @@ class Response(models.Model):
 class Hint(models.Model):
     """ A class to represent a hint to a puzzle """
 
-    puzzle = models.ForeignKey(Puzzle, on_delete=models.CASCADE,
+    puzzle = models.ForeignKey(
+        Puzzle,
+        on_delete=models.CASCADE,
         help_text="The puzzle that this hint is related to")
-    team = models.ForeignKey(Team, on_delete=models.CASCADE,
+    team = models.ForeignKey(
+        Team,
+        on_delete=models.CASCADE,
         help_text="The team that requested the hint")
-    request = models.CharField(max_length=400,
+    request = models.CharField(
+        max_length=400,
         help_text="The text of the request for the hint")
     request_time = models.DateTimeField(
         help_text="Hint request time")
-    response = models.CharField(max_length=400, blank=True,
+    response = models.CharField(
+        max_length=400,
+        blank=True,
         help_text="The text of the response to the hint request")
-    response_time = models.DateTimeField(null=True, blank=True,
+    response_time = models.DateTimeField(
+        null=True,
+        blank=True,
         help_text="Hint response time")
     last_modified_time = models.DateTimeField(
         help_text="Last time of modification")
@@ -445,7 +557,9 @@ class Hint(models.Model):
 @python_2_unicode_compatible
 class HintUnlockPlan(models.Model):
     """ A class to represent when Teams are given hints """
-    hunt = models.ForeignKey(Hunt, on_delete=models.CASCADE,
+    hunt = models.ForeignKey(
+        Hunt,
+        on_delete=models.CASCADE,
         help_text="The hunt that this hint unlock plan refers to")
 
     TIMED_UNLOCK = 'TIM'
@@ -469,7 +583,8 @@ class HintUnlockPlan(models.Model):
     unlock_parameter = models.IntegerField(
         help_text="Parameter (Time / Interval / Solves)")
 
-    num_triggered = models.IntegerField(default=0,
+    num_triggered = models.IntegerField(
+        default=0,
         help_text="Number of times this Unlock Plan has given a hint")
 
     def reset_plan(self):
