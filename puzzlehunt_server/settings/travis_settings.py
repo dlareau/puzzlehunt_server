200f3a0bd0edd5a7409f36769cc8401b468bdb64
Dillon Lareau <dlareau@cmu.edu>
2020-03-11 09:51:22 -0400

    Fix database for testing environment

--- puzzlehunt_server/settings/travis_settings.py
+++ puzzlehunt_server/settings/travis_settings.py
@@ -7,12 +7,11 @@ DEBUG = False
 SECRET_KEY = '$1B&VUf$OdUEfMJXd40qdakA36@%2NE_41Dz9tFs6l=z4v_3P-'
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'puzzlehunt_db',
         'HOST': '127.0.0.1',
         'USER': 'root',
         'PASSWORD': '',
-        'OPTIONS': {'charset': 'utf8mb4'},
     }
 }
 INTERNAL_IPS = ''
